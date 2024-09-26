99dda7fb7485b474b0594a978e6a63c6cd595a68
kyungmin <02dlrudals@gmail.com>
2024-09-26 23:32:41 +0900

    feat: Arcodion 구현

--- 9oormthonUNIV_JNU/src/App.tsx
+++ 9oormthonUNIV_JNU/src/App.tsx
@@ -3,10 +3,11 @@ import Footer from "./components/common/organisms/Footer";
 import styled from "styled-components";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import Accordion from "./components/common/atoms/Arcordion";
 
 const MainContainer = styled.div`
   flex-direction: column;
-  padding: 100px 100px;
+  padding: 50px 100px;
   flex-direction: column;
   gap: 40px;
   margin-bottom: 336px;
@@ -20,6 +21,9 @@ const App = () => {
         <Routes>
           <Route path="/" element={<HomePage />} />
         </Routes>
+        <Accordion title="2기 모집글 확인하기">
+          https://account.everytime.kr/login?redirect_uri=https%3A%2F%2Feverytime.kr%2F375208%2Fv%2F348827899
+        </Accordion>
       </MainContainer>
       <Footer />
     </Router>
