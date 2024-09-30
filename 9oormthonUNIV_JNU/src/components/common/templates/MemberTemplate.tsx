a5b8d6089bf32cc656e41ea35ac616f24d1e3703
kyungmin <02dlrudals@gmail.com>
2024-10-01 02:16:19 +0900

    fix: MemberPage 스타일 수정

--- 9oormthonUNIV_JNU/src/components/common/templates/MemberTemplate.tsx
+++ 9oormthonUNIV_JNU/src/components/common/templates/MemberTemplate.tsx
@@ -11,36 +11,37 @@ const MemberTemplateContainer = styled.div`
   align-items: center;
   width: 100%;
   box-sizing: border-box;
+  max-width: 1200px; /* 추가 */
+  margin: 0 auto; /* 추가 */
 
   .member_content {
     padding: 80px;
-    marginbotton:71px;
+    margin-bottom: 71px;
   }
 
   .filter_button {
-  display: flex;
-  flex-direction:row;
-  padding-bottom: 100px;
-  gap: 21px;
+    display: flex;
+    flex-direction: row;
+    gap: 21px;
   }
 `;
 
 const MemberTemplate = () => {
-    return (
-        <MemberTemplateContainer>
-            <div className="member_content">
-                <CustomText textStyle="h1">Member</CustomText>
-            </div>
-            <div className="filter_button">
-                <FilterButton filterType="파트별" options={["전체", "PM", "PD", "FE", "BE"]} />
-                <FilterButton filterType="기수별" options={["전체", "2기", "3기"]} />
-
-            </div>
-
-            <MemberBoxes MemberData={MemberData} />
-        </MemberTemplateContainer>
-    );
-
+  return (
+    <MemberTemplateContainer>
+      <div className="member_content">
+        <CustomText textStyle="h1">Member</CustomText>
+      </div>
+      <div className="filter_button">
+        <FilterButton
+          filterType="파트별"
+          options={["전체", "PM", "PD", "FE", "BE"]}
+        />
+        <FilterButton filterType="기수별" options={["전체", "2기", "3기"]} />
+      </div>
+      <MemberBoxes MemberData={MemberData} />
+    </MemberTemplateContainer>
+  );
 };
 
 export default MemberTemplate;
