7c4971a89ab631fa452298eb790cae8c95ba5f7e
kyungmin <02dlrudals@gmail.com>
2024-10-08 15:01:43 +0900

    feat: Login API 로직

--- 9oormthonUNIV_JNU/src/components/common/organisms/LoginForm.tsx
+++ 9oormthonUNIV_JNU/src/components/common/organisms/LoginForm.tsx
@@ -3,16 +3,16 @@ import styled from "styled-components";
 import CustomText from "../atoms/CustomText";
 import CustomButton from "../atoms/CustomButton";
 import { useNavigate } from "react-router-dom";
+import { instance } from "../../../apis/instance";
+import { useState } from "react";
 
 const LoginFormContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  align-items: center;
 
   .login {
     display: flex;
-    width: 100%;
     justify-content: center;
     margin-bottom: 75px;
   }
@@ -21,40 +21,94 @@ const LoginFormContainer = styled.div`
     display: flex;
     flex-direction: column;
     gap: 10px;
-    width: 50%;
-    margin-bottom: 45px;
+    margin-bottom: 20px;
+  }
+
+  .login_error {
+    display: flex;
   }
 
   .login_button {
     display: flex;
-    width: 50%;
+    margin-top: 20px;
     margin-bottom: 22px;
+  }
+
+  .login_signup {
+    display: flex;
+    justify-content: center;
   }
 `;
 
 const LoginForm = () => {
   const nav = useNavigate();
+
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
+  const [error, setError] = useState<string | null>();
+
+  const handleLogin = async () => {
+    // 이메일과 비밀번호가 입력되지 않았을 경우 에러 메시지 설정
+    if (!email || !password) {
+      setError("아이디와 비밀번호를 입력해주세요.");
+      return; // 입력이 없으면 API 요청을 중단
+    }
+    try {
+      const response = await instance.post("/api/user/login", {
+        email,
+        password,
+      });
+
+      if (response.data.success) {
+        nav("/");
+      } else {
+        setError("아이디 또는 비밀번호가 틀렸습니다.");
+      }
+    } catch {
+      setError("아이디 또는 비밀번호가 틀렸습니다.");
+    }
+  };
+
   return (
     <LoginFormContainer>
       <div className="login">
         <CustomText textStyle="h2">LOGIN</CustomText>
       </div>
       <div className="login_input">
-        <CustomInput placeholder="Email" type="email" />
-        <CustomInput placeholder="Password" type="password" />
+        <CustomInput
+          placeholder="Email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <CustomInput
+          placeholder="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
+      {error && (
+        <div className="login_error">
+          <CustomText textStyle="nav" color="#FF6D57">
+            {error}
+          </CustomText>
+        </div>
+      )}
       <div className="login_button">
-        <CustomButton textStyle="b3" textColor="#5E5E5E">
+        <CustomButton textStyle="b3" textColor="#5E5E5E" onClick={handleLogin}>
           LOGIN
         </CustomButton>
       </div>
-      <CustomText
-        color=" #797979"
-        textStyle="nav"
-        onClick={() => nav("/signup")}
-      >
-        회원가입
-      </CustomText>
+      <div className="login_signup">
+        <CustomText
+          color=" #797979"
+          textStyle="nav"
+          onClick={() => nav("/signup")}
+        >
+          회원가입
+        </CustomText>
+      </div>
     </LoginFormContainer>
   );
 };
