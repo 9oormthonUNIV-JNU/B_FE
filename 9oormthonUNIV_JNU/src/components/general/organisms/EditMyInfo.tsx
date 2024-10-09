133a7835b474eabc044b2ab8fb6d9ca6043cbde9
godong <yunsun4830@gmail.com>
2024-10-09 11:16:54 +0900

    fix:EditMyInfo  api

--- 9oormthonUNIV_JNU/src/components/general/organisms/EditMyInfo.tsx
+++ 9oormthonUNIV_JNU/src/components/general/organisms/EditMyInfo.tsx
@@ -138,7 +138,8 @@ const EditMyInfo: React.FC = () => {
   const handleDeleteImage = () => {
     setUserProfile((prev) => ({ ...prev, profileImage: "" }));
   };
-
+  
+//마이페이지 세부정보 수정 api
   const handleSaveProfile = async () => {
     try {
       const response = await instance.patch("/api/user/{user_id}", {
